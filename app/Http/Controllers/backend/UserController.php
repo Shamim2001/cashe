1b008395ad897e56d04e530bce339a3bc764620e
shamim2001 <shamimahmedridoy2016@gmail.com>
2022-06-18 11:13:21 +0600

    user profile creation

--- app/Http/Controllers/backend/UserController.php
+++ app/Http/Controllers/backend/UserController.php
@@ -11,15 +11,31 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class UserController extends Controller {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index() {
         $users = User::orderBy( 'id' )->latest()->paginate( 15 );
         return view( 'backend.user.index' )->with( 'users', $users );
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create() {
         return view( 'backend.user.create' );
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store( Request $request ) {
         $request->validate( [
             'name'       => 'required|max:100',
@@ -51,10 +67,33 @@ class UserController extends Controller {
         return redirect()->route( 'users.index' )->with( 'success', 'User Created' );
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show( User $user ) {
+         return view('backend.user.show')->with('user', $user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit( User $user ) {
         return view( 'backend.user.edit' )->with( 'user', $user );
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update( Request $request, User $user ) {
         $request->validate( [
             'name'       => 'required|max:100',
@@ -69,7 +108,7 @@ class UserController extends Controller {
         if ( !empty( $request->file( 'thumbnail' ) ) ) {
             $thumb = time() . '-' . $request->file( 'thumbnail' )->getClientOriginalName();
             $thumb = str_replace( ' ', '-', $thumb );
-            Storage::delete('public/uploads/clients'.$thumb);
+            Storage::delete( 'public/uploads/clients' . $thumb );
 
             $request->file( 'thumbnail' )->storeAs( 'public/uploads/clients', $thumb );
         }
@@ -89,22 +128,30 @@ class UserController extends Controller {
         return redirect()->route( 'users.index' )->with( 'success', 'User Updated' );
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy( User $user ) {
-
-
         $user->delete();
         return redirect()->route( 'users.index' )->with( 'success', 'User Deleted!' );
     }
 
-     // Email send
-    public function sendEmail(User $user)
-    {
-        Mail::send('emails.email', [''], function ($message) {
-            $message->from('john@johndoe.com', 'John Doe');
-            $message->to('john@johndoe.com', 'John Doe');
-            $message->subject('Test Mail');
-        });
-        return redirect()->route('users.index')->with('success', 'Email Sent');
+    // Email send
+    public function sendEmail( User $user ) {
+        Mail::send( 'emails.email', [''], function ( $message ) {
+            $message->from( 'john@johndoe.com', 'John Doe' );
+            $message->to( 'john@johndoe.com', 'John Doe' );
+            $message->subject( 'Test Mail' );
+        } );
+
+        $user->update([
+            'mail_sent' => 'yes'
+        ]);
+
+        return redirect()->route( 'users.index' )->with( 'success', 'Email Sent' );
     }
 
 }
